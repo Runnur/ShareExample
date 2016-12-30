7704c79c20718126d163e41158a48015c7887380
Denis Yurchenko <yurdenis@gmail.com>
2016-12-30 22:57:18 +0600

    First add files

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+// Define DB params
+
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "12345678");
+define("DB_NAME", "shareboard");
+
+// Define URL
+define("ROOT_PATH", "/");
+define("ROOT_URL", "http://iosdevprogs.dev/")
+
+?>
